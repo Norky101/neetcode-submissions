130b1a9363ac4106ad4d3abb00e0eefac5d5b202
Noah Pilkington <82910305+Norky101@users.noreply.github.com>
2026-05-12 15:31:41 -0500

    Add: string-encode-and-decode - submission-10

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-10.py
@@ -0,0 +1,36 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        result = ""
+        for s in strs: 
+            result += str(len(s)) + "#" + s
+        return result
+
+        # "5#Hello5#World"
+
+    def decode(self, s: str) -> List[str]:
+
+        result = []
+        word_chunk = 0 
+
+        while word_chunk < len(s): 
+            delimiter = word_chunk
+
+            while s[delimiter] != "#": 
+                delimiter +=1 
+            
+            # hit the delimiter 
+            word_len = int(s[word_chunk:delimiter])
+            word_start = delimiter + 1 
+            word_end = word_start + word_len 
+
+            result.append(s[word_start:word_end])
+        
+            word_chunk = word_end
+
+        return result
+
+
+
+
+
